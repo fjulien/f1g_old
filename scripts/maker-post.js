336938fe89c801a0cd5550f8c41180d83ecb2934
fjulien <39367571+fjulien@users.noreply.github.com>
2023-05-12 11:01:38 +0200

    Update maker-post.js

--- scripts/maker-post.js
+++ scripts/maker-post.js
@@ -1,31 +1,49 @@
 import fs from "fs";
 
 export default class MakerPost {
-  _title;
-  resume;
-  newPost = fs.createWriteStream("remove-me.md", {
-    flags: "w", // 'a' means appending (old data will be preserved)
+  #title;
+  #introduction;
+  #sections = [];
+  newPost;
+  
+  constructor(fileName){
+    this.newPost = fs.createWriteStream(`${fileName}.md`, {
+    flags: "w",
   });
+  }
 
   title(title) {
-    this._emptyChecking(title);
-    this._title = title;
+    this.#emptyChecking(title);
+    this.#title = title;
     return this;
   }
+  
+  introduction(introduction){
+    return this;
+  } // who what when where why
 
   publish() {
     this.newPost.write(`# ${this._title}`);
     this.newPost.end();
   }
+  
+  addSection(title, content, image){
+  return this;
+  }
 
-  _write(line) {
+  #write(line) {
     this.newPost.write(`\n\n${line}`);
   }
 
   // Functional errors
-  _emptyChecking(content) {
+  #emptyChecking(content) {
     if (!content) {
       throw new Error("Content is empty");
     }
   }
+  #sizeTitleChecking(title){} // 6 à 12 mots &  max 90 char
+  #sizePhraseChecking(content){} // 15 a 20 mots
+  #sizePostChecking(content){} // 500 -1000 à 1000 à 2000
+  #firstPersonChecking(content){} // utilisation de la première personnee
+ 
 }
